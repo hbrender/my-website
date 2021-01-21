bca8d1b6ba14382a36a5717a984d8ac04b36a763
hbrender <hbrender615@gmail.com>
2021-01-20 20:14:45 -0800

    Footer and navbar reactive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 function Footer() {
   return (
     <footer>
-      <a target="_blank" class="social-icon" href="https://www.linkedin.com/in/hanna-brender-75a194149/"><i class="fab fa-lg fa-linkedin"></i></a>
-      <a target="_blank" class="social-icon" href="https://github.com/hbrender"><i class="fab fa-lg fa-github"></i></a>
+      <a target="_blank" rel="noreferrer" className="social-icon" href="https://www.linkedin.com/in/hanna-brender-75a194149/"><i className="fab fa-lg fa-linkedin"></i></a>
+      <a target="_blank" rel="noreferrer" className="social-icon" href="https://github.com/hbrender"><i className="fab fa-lg fa-github"></i></a>
     
       <p>Designed & Developed by Hanna Brender</p>
     </footer>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,16 +13,12 @@ function Navbar() {
       <nav className={burgerClicked ? "nav-active" : ""}>
         <div className="brand">Hanna Brender</div>
         <ul className={burgerClicked ? "nav-links-active nav-links" : "nav-links"}>
-          <li><a href="#news">About</a></li>
-          <li><a href="#contact">Projects</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#projects">Projects</a></li>
           <li><a href="#contact">Contact</a></li>
           <li><a className="resume" href="#resume">Resume</a></li>
         </ul>
-        <div onClick={handleClick} className="burger">
-          <div className="line1"></div>
-          <div className="line2"></div>
-          <div className="line3"></div>
-        </div>
+        <div onClick={handleClick}><i className="fas fa-bars"></i></div>
       </nav>
     </div>
   )
