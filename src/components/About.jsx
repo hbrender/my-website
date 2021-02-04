32f8bafe9fe051de8f1efa05e00735d9a513018d
hbrender <hbrender615@gmail.com>
2021-02-03 20:05:40 -0800

    Add content and style about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 
 function About() {
   return (
-    <div className="about">
+    <div id="about" className="dark">
       <h2><span className="first-letter">a</span>bout</h2>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <div className="about-content">
+        <p>Hi! My name is Hanna Brender and I am from the Seattle, WA area. I recently graduated from <a href="https://www.gonzaga.edu/">Gonzaga University</a> in May 2020 with a Bachelor's degree in Computer Science.</p>
+        <p>I have industry experience as a Java Developer and Android App Developer through my internship with <a href="https://www.boeing.com/">Boeing</a> and my contract work with <a href="https://gravityjack.com/">Gravity Jack</a>. In my free time I have taken courses on Web Development where I have learned HTML, CSS, JavaScript, React, MongoDB and Express.</p>
+        <p>When I am not coding, you can catch me drawing, painting, crafting or walking my dog Millie! </p>
+      </div>
+      <div className="about-img">
+        <img src="images/profile.png" alt="profile picture"></img>
+      </div>
     </div>
   );
 }
